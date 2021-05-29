0bdcdd5a5143f2560a4eddab912f5eeade429560
Ricardo Schleder Tozetto <ricardo@optim.eng.br>
2021-05-29 15:43:17 -0300

    Header Principal criado

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 
-import {Container} from './styles';
+import emojis from '../../utils/emojis';
+
+import {Container, Profile, Welcome, UserName} from './styles';
 
 const MainHeader: React.FC = () => {
+
+    const emoji = useMemo( () => {
+        const indice = Math.floor(Math.random() * emojis.length);
+        return emojis[indice];
+    },[]);
     return(
         <Container>
-            <h1>MainHeader</h1>
+            <h1>Toglle</h1>
+            <Profile>
+                <Welcome>
+                    Olá, {emoji}
+                </Welcome>
+                <UserName>
+                    Ricardo Schleder Tozetto
+                </UserName>
+            </Profile>
         </Container>
     );
 }
